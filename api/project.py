2feef142b712c8055f1fc71b8c2d2c8fe381d989
somnathsynergic <somnath@synergicsoftek.in>
2024-07-22 15:05:05 +0530

    commit with open project

--- api/project.py
+++ api/project.py
@@ -34,7 +34,7 @@ class Project(BaseModel):
      erection_responsibility:str
      warranty:str
      project_status:str
-     proj_manager:str
+     proj_manager:int
      proj_remarks:str
      user:str
      
@@ -45,10 +45,11 @@ class GetProject(BaseModel):
 @projectRouter.post('/addproject')
 async def addproject(dt:Project):
     print(dt)
+    res_dt={}
     current_datetime = datetime.now()
     formatted_dt = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
     fields= f'proj_id,proj_name,client_id,order_id,order_date,proj_location,proj_addr,proj_desc,proj_order_val,proj_end_user,proj_consultant,epc_contractor,proj_manufacturer,price_basis,extra,ld_clause,erection_responsibility,warranty,project_status,created_by,created_at'
-    values = f'"{dt.proj_id}","{dt.proj_name}","{dt.client_id}","{dt.order_id}","{dt.order_date}","{dt.proj_location}",{dt.proj_addr}","{dt.proj_desc}","{dt.proj_order_val}","{dt.proj_end_user}","{dt.proj_consultant}","{dt.epc_contractor}","{dt.proj_manufacturer}","{dt.price_basis}","{dt.extra}","{dt.ld_clause}","{dt.erection_responsibility}","{dt.warranty}","{dt.project_status}","{dt.user}","{formatted_dt}"'
+    values = f'"{dt.proj_id}","{dt.proj_name}","{dt.client_id}","{dt.order_id}","{dt.order_date}","{dt.proj_location}","{dt.proj_addr}","{dt.proj_desc}","{dt.proj_order_val}","{dt.proj_end_user}","{dt.proj_consultant}","{dt.epc_contractor}","{dt.proj_manufacturer}","{dt.price_basis}","{dt.extra}","{dt.ld_clause}","{dt.erection_responsibility}","{dt.warranty}","{dt.project_status}","{dt.user}","{formatted_dt}"'
     table_name = "td_project"
     whr =  None
     flag = 1 if dt.id>0 else 0
@@ -73,6 +74,9 @@ async def addproject(dt:Project):
                 res_dt = {"suc": 1, "msg": "Project inserted successfully!"}
             else:
                 res_dt = {"suc": 0, "msg": "Error while inserting!"}
+        else:
+            res_dt = {"suc": 0, "msg": "Error while inserting!"}
+    
     else:
         print(flag)
         fields=f'proj_id="{dt.proj_id}",proj_name="{dt.proj_name}",client_id="{dt.client_id}",order_date="{dt.order_date}",proj_location="{dt.proj_location}",proj_addr="{dt.proj_addr}",proj_desc="{dt.proj_desc}",proj_order_val="{dt.proj_order_val}",proj_end_user="{dt.proj_end_user}",proj_consultant="{dt.proj_consultant}",epc_contractor="{dt.epc_contractor}",proj_manufacturer="{dt.proj_manufacturer}",price_basis="{dt.price_basis}",extra="{dt.extra}",ld_clause="{dt.ld_clause}",erection_responsibility="{dt.erection_responsibility}",warranty="{dt.warranty}",project_status="{dt.project_status}",modified_by="{dt.user}",modified_at="{formatted_dt}"'
@@ -97,6 +101,10 @@ async def addproject(dt:Project):
                 res_dt = {"suc": 1, "msg": "Project updated successfully!"}
             else:
                 res_dt = {"suc": 0, "msg": "Error while updating!"}
+        else:
+            res_dt = {"suc": 0, "msg": "Error while updating!"}
+
+
     return res_dt
 
 
@@ -106,7 +114,7 @@ async def getproject(id:GetProject):
     print(id.id)
     res_dt = {}
     # SELECT @a:=@a+1 serial_number, busi_act_name FROM md_busi_act, (SELECT @a:= 0) AS a
-    select = "@a:=@a+1 serial_number, p.proj_id,p.proj_name,p.client_id,p.order_id,p.order_date,p.proj_location,p.proj_addr,p.proj_desc,p.proj_order_val,p.proj_end_user,p.proj_consultant,p.epc_contractor,p.proj_manufacturer,p.price_basis,p.extra,p.ld_clause,p.erection_responsibility,p.warranty,p.project_status,pa.proj_manager,u.user_name,r.proj_remarks,created_by,created_at, modified_by,modified_at,p.sl_no"
+    select = "@a:=@a+1 serial_number, p.proj_id,p.proj_name,p.client_id,p.order_id,p.order_date,p.proj_location,p.proj_addr,p.proj_desc,p.proj_order_val,p.proj_end_user,p.proj_consultant,p.epc_contractor,p.proj_manufacturer,p.price_basis,p.extra,p.ld_clause,p.erection_responsibility,p.warranty,p.project_status,pa.proj_manager,u.user_name as proj_manager_name,r.proj_remarks,p.created_by,p.created_at, p.modified_by,p.modified_at,p.sl_no"
     # select = "@a:=@a+1 serial_number, *"
     schema = "td_project p,td_project_assign pa,td_project_remarks r,md_user u,(SELECT @a:= 0) AS a"
     where = f"pa.proj_id=p.proj_id and r.proj_id = p.proj_id and pa.proj_manager=u.sl_no and p.sl_no='{id.id}'" if id.id>0 else f"pa.proj_id=p.proj_id and r.proj_id = p.proj_id and pa.proj_manager=u.sl_no"
