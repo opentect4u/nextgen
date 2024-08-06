13eae6bd586e7661cbad3f93fd51156761afc7a2
somnathsynergic <somnath@synergicsoftek.in>
2024-08-06 12:19:24 +0530

    commit with po

--- api/po.py
+++ api/po.py
@@ -153,8 +153,8 @@ async def getprojectpoc(id:GetPo):
     print(id.id)
     res_dt = {}
 
-    select = "@a:=@a+1 serial_number,b.po_id,b.po_date,b.po_type,b.project_id,p.proj_name,b.vendor_id,b.created_by,b.created_at,b.created_by,b.created_at,b.modified_by,b.modified_at,b.sl_no"
-    schema = "td_po_basic b, td_project p,(SELECT @a:= 0) AS a"
+    select = "@a:=@a+1 serial_number,b.po_id,b.po_date,IF(b.po_type='P','Project-Specific', IF(b.po_type='G', 'General','')) po_type,b.project_id,p.proj_name,b.vendor_id,b.created_by,b.created_at,b.created_by,b.created_at,b.modified_by,b.modified_at,b.sl_no"
+    schema = "td_po_basic b,td_project p,(SELECT @a:= 0) AS a"
     where = f"b.sl_no='{id.id}' and p.proj_id=b.project_id" if id.id>0 else "p.sl_no=b.project_id"
     order = "ORDER BY b.created_at DESC"
     flag = 0 if id.id>0 else 1
