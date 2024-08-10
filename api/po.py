6cef92a4c4591d2f1fd19b3cf786246139ef4bfc
somnathsynergic <somnath@synergicsoftek.in>
2024-08-10 15:54:10 +0530

    commit with try catch

--- api/po.py
+++ api/po.py
@@ -106,23 +106,26 @@ async def addpo(data:PoModel):
     result = await db_Insert(table_name, fields, values, whr, flag)
     lastID=data.sl_no if data.sl_no>0 else result["lastId"]
     print(data.item_dtl,type(data.item_dtl))
-    if type(data.item_dtl) is not None and len(data.item_dtl)>0:
-        for c in data.item_dtl:
-            fields1= f'item_id="{c.item_name}",quantity="{c.qty}",item_rt="{c.rate}",discount="{c.disc}",unit_id="{c.unit}",cgst_id="{c.CGST}", sgst_id="{c.SGST}",igst_id="{c.IGST}",delivery_dt="{c.delivery_date}",modified_by="{data.user}",modified_at="{formatted_dt}"' if c.sl_no > 0 else f'po_sl_no,item_id,quantity,item_rt,discount,unit_id,cgst_id,sgst_id,igst_id,delivery_dt,created_by,created_at'
-            values1 = f'"{lastID}","{c.item_name}","{c.qty}","{c.rate}","{c.disc}","{c.unit}","{c.CGST}","{c.SGST}","{c.IGST}","{c.delivery_date}","{data.user}","{formatted_dt}"'
-            table_name1 = "td_po_items"
-            whr1=  f'sl_no="{c.sl_no}" and po_sl_no="{data.sl_no}"' if c.sl_no > 0 else None
-            flag1 = 1 if c.sl_no>0 else 0
-            result1 = await db_Insert(table_name1, fields1, values1, whr1, flag1)
-            item_save=1 if result1['suc']>0 else 0
-    else:
-            fields1= f'po_sl_no,created_by,created_at'
-            values1 = f'"{lastID}","{data.user}","{formatted_dt}"'
-            table_name1 = "td_po_items"
-            whr1= None
-            flag1 = 0
-            result1 = await db_Insert(table_name1, fields1, values1, whr1, flag1)
-            item_save=1 if result1['suc']>0 else 0
+    try:
+        if type(data.item_dtl) is not None and len(data.item_dtl)>0:
+            for c in data.item_dtl:
+                fields1= f'item_id="{c.item_name}",quantity="{c.qty}",item_rt="{c.rate}",discount="{c.disc}",unit_id="{c.unit}",cgst_id="{c.CGST}", sgst_id="{c.SGST}",igst_id="{c.IGST}",delivery_dt="{c.delivery_date}",modified_by="{data.user}",modified_at="{formatted_dt}"' if c.sl_no > 0 else f'po_sl_no,item_id,quantity,item_rt,discount,unit_id,cgst_id,sgst_id,igst_id,delivery_dt,created_by,created_at'
+                values1 = f'"{lastID}","{c.item_name}","{c.qty}","{c.rate}","{c.disc}","{c.unit}","{c.CGST}","{c.SGST}","{c.IGST}","{c.delivery_date}","{data.user}","{formatted_dt}"'
+                table_name1 = "td_po_items"
+                whr1=  f'sl_no="{c.sl_no}" and po_sl_no="{data.sl_no}"' if c.sl_no > 0 else None
+                flag1 = 1 if c.sl_no>0 else 0
+                result1 = await db_Insert(table_name1, fields1, values1, whr1, flag1)
+                item_save=1 if result1['suc']>0 else 0
+    except:
+        print('Error')
+    # else:
+    #         fields1= f'po_sl_no,created_by,created_at'
+    #         values1 = f'"{lastID}","{data.user}","{formatted_dt}"'
+    #         table_name1 = "td_po_items"
+    #         whr1= None
+    #         flag1 = 0
+    #         result1 = await db_Insert(table_name1, fields1, values1, whr1, flag1)
+    #         item_save=1 if result1['suc']>0 else 0
           
     fields2= f'price_basis="{data.price_basis}",price_basis_desc="{data.price_basis_desc}",packing_fwd_val="{data.packing_fwd_val}",packing_fwd_extra="{data.packing_fwd_extra}",packing_fwd_extra_val="{data.packing_fwd_extra_val}",freight_ins="{data.freight_ins}",freight_ins_val="{data.freight_ins_val}",test_certificate="{data.test_certificate}",test_certificate_desc="{data.test_certificate_desc}",ld_date="{data.ld_date}",ld_date_desc="{data.ld_date_desc}",ld_val="{data.ld_val}",ld_val_desc="{data.ld_val_desc}",ld_val_per="{data.ld_val_per}",min_per="{data.min_per}",warranty_guarantee="{data.warranty_guaranty}",duration="{data.duration}",duration_value="{data.duration_value}",o_m_manual="{data.o_m_manual}",operation_installation_desc="{data.operation_installation_desc}",packing_type="{data.packing_type}",o_m_desc="{data.o_m_desc}",operation_installation="{data.operation_installation}",manufacture_clearance="{data.manufacture_clearance}",manufacture_clearance_desc="{data.manufacture_clearance_desc}",modified_by="{data.user}",modified_at="{formatted_dt}"' if data.sl_no > 0 else f'po_sl_no,price_basis,price_basis_desc,packing_fwd_val,freight_ins,fright_ins_val,test_certificate,test_certificate_desc,ld_date,ld_date_desc,ld_val,ld_val_desc,ld_val_per,min_per,warranty_guarantee,duration,duration_value,o_m_manual,operation_installation_desc,packing_type,o_m_desc,operation_installation,manufacture_clearance,manufacture_clearance_desc,created_by,created_at'
     values2 = f'"{lastID}","{data.price_basis}","{data.price_basis_desc}","{data.packing_fwd_val}","{data.packing_fwd_extra}","{data.packing_fwd_extra_val}","{data.freight_ins}","{data.freight_ins_val}","{data.test_certificate}","{data.test_certificate_desc}","{data.ld_date}","{data.ld_date_desc}","{data.ld_val}","{data.ld_val_desc}","{data.ld_val_per}","{data.min_per}","{data.warranty_guaranty}","{data.duration}","{data.duration_value}","{data.o_m_manual}","{data.operation_installation_desc}","{data.packing_type}","{data.o_m_desc}","{data.operation_installation}","{data.manufacture_clearance}","{data.manufacture_clearance_desc}","{data.user}","{formatted_dt}"'
@@ -133,24 +136,26 @@ async def addpo(data:PoModel):
     result2 = await db_Insert(table_name2, fields2, values2, whr2, flag2)
 
     print(data.payment_terms,type(data.payment_terms))
-
-    if type(data.payment_terms) is not None and len(data.payment_terms)>0:
-        for c in data.payment_terms:
-            fields3= f'stage_no="{c.stage}",terms_dtls="{c.term}",modified_by="{data.user}",modified_at="{formatted_dt}"' if c.sl_no > 0 else f'po_sl_no,stage_no,terms_dtls,created_by,created_at'
-            values3 = f'"{lastID}","{c.stage}","{c.term}","{data.user}","{formatted_dt}"'
-            table_name3 = "td_po_payment_dtls"
-            whr3=  f'sl_no="{c.sl_no}" and po_sl_no="{data.sl_no}"' if c.sl_no > 0 else None
-            flag3 = 1 if c.sl_no>0 else 0
-            result3 = await db_Insert(table_name3, fields3, values3, whr3, flag3)
-            payment_save=1 if result3['suc']>0 else 0
-    else:
-        fields3= f'po_sl_no,created_by,created_at'
-        values3 = f'"{lastID}","{data.user}","{formatted_dt}"'
-        table_name3 = "td_po_payment_dtls"
-        whr3=None
-        flag3 = 0
-        result3 = await db_Insert(table_name3, fields3, values3, whr3, flag3)
-        payment_save=1 if result3['suc']>0 else 0
+    try:
+        if type(data.payment_terms) is not None and len(data.payment_terms)>0:
+            for c in data.payment_terms:
+                fields3= f'stage_no="{c.stage}",terms_dtls="{c.term}",modified_by="{data.user}",modified_at="{formatted_dt}"' if c.sl_no > 0 else f'po_sl_no,stage_no,terms_dtls,created_by,created_at'
+                values3 = f'"{lastID}","{c.stage}","{c.term}","{data.user}","{formatted_dt}"'
+                table_name3 = "td_po_payment_dtls"
+                whr3=  f'sl_no="{c.sl_no}" and po_sl_no="{data.sl_no}"' if c.sl_no > 0 else None
+                flag3 = 1 if c.sl_no>0 else 0
+                result3 = await db_Insert(table_name3, fields3, values3, whr3, flag3)
+                payment_save=1 if result3['suc']>0 else 0
+    except:
+        print('Error')
+    # else:
+    #     fields3= f'po_sl_no,created_by,created_at'
+    #     values3 = f'"{lastID}","{data.user}","{formatted_dt}"'
+    #     table_name3 = "td_po_payment_dtls"
+    #     whr3=None
+    #     flag3 = 0
+    #     result3 = await db_Insert(table_name3, fields3, values3, whr3, flag3)
+    #     payment_save=1 if result3['suc']>0 else 0
 
 
     fields4= f'ship_to="{data.ship_to}",ware_house_flag="{data.warehouse_flag}",po_notes="{data.po_notes}",modified_by="{data.user}",modified_at="{formatted_dt}"' if data.sl_no > 0 else f'po_sl_no,bill_to,ship_to,ware_house_flag,po_notes,created_by,created_at'
