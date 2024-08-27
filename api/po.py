6689ca7b3a3442bb66ba2633597864a3f67b7885
somnathsynergic <somnath@synergicsoftek.in>
2024-08-27 16:52:29 +0530

    commit with amend

--- api/po.py
+++ api/po.py
@@ -299,6 +299,19 @@ async def getprojectpoc(id:GetPo):
     result = await db_select(select, schema, where, order, flag)
     # print(result, 'RESULT')
     return result
+@poRouter.post('/getpoitemfortc')
+async def getprojectpoc(id:GetPo):
+    # print(id.id)
+    res_dt = {}
+
+    select = "i.sl_no,i.po_sl_no,i.item_id,i.quantity,i.item_rt,i.discount_percent,i.discount,p.prod_name"
+    schema = "td_po_items"
+    where = f"i.po_sl_no='{id.id}' and i.sl_no=p.sl_no" if id.id>0 else ""
+    order = ""
+    flag = 1 if id.id>0 else 0
+    result = await db_select(select, schema, where, order, flag)
+    # print(result, 'RESULT')
+    return result
 
 @poRouter.post('/getpoterms')
 async def getprojectpoc(id:GetPo):
