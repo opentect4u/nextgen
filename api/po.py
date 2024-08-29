9765922053a09215ceb976803fa02b733e60411d
somnathsynergic <somnath@synergicsoftek.in>
2024-08-29 15:41:47 +0530

    commit with search

--- api/po.py
+++ api/po.py
@@ -1003,7 +1003,7 @@ async def gettcbypo(id:GetPo):
     schema = "td_test_cert"
     where = f"sl_no='{id.id}'" if id.id>0 else f""
     order = "ORDER BY created_at DESC"
-    flag = 1 if id.id>0 else 0
+    flag = 0 if id.id>0 else 1
     result = await db_select(select, schema, where, order, flag)
     print(result, 'RESULT')
     return result
@@ -1017,7 +1017,7 @@ async def gettcbypo(id:GetPo):
     schema = "td_mdcc"
     where = f"sl_no='{id.id}'" if id.id>0 else f""
     order = "ORDER BY created_at DESC"
-    flag = 1 if id.id>0 else 0
+    flag = 0 if id.id>0 else 1
     result = await db_select(select, schema, where, order, flag)
     print(result, 'RESULT')
     return result
