d8f1c1a87dbc5e8eeb918bcc253d4ce881d0163b
somnathsynergic <somnath@synergicsoftek.in>
2024-08-29 16:13:54 +0530

    commit with search

--- api/po.py
+++ api/po.py
@@ -1061,7 +1061,7 @@ async def gettcbypo(id:srcGetByItem):
     select = "item,qty,status"
     schema = "td_test_cert"
     where = f"po_no='{id.po}' and item={id.item}"
-    order = "ORDER BY test.created_at DESC"
+    order = "ORDER BY created_at DESC"
     flag = 1
     result = await db_select(select, schema, where, order, flag)
     print(result, 'RESULT')
@@ -1072,9 +1072,9 @@ async def gettcbypo(id:srcGetByItem):
     print('I am logging in!')
     print(id.id)
     res_dt = {}
-    select = "*"
+    select = "item,qty,status"
     schema = "td_mdcc"
-    where = f"sl_no='{id.id}'" if id.id>0 else f""
+    where = f"po_no='{id.po}' and item={id.item}"
     order = "ORDER BY created_at DESC"
     flag = 0 if id.id>0 else 1
     result = await db_select(select, schema, where, order, flag)
