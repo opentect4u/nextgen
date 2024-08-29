d23638d0658b26603ac9619de7f407447ab2285f
somnathsynergic <somnath@synergicsoftek.in>
2024-08-29 15:40:59 +0530

    commit with search

--- api/po.py
+++ api/po.py
@@ -1000,8 +1000,8 @@ async def gettcbypo(id:GetPo):
     print(id.id)
     res_dt = {}
     select = "*"
-    schema = "td_test_cert,(SELECT @a:= 0) AS a"
-    where = f"po_no='{id.id}'" if id.id>0 else f""
+    schema = "td_test_cert"
+    where = f"sl_no='{id.id}'" if id.id>0 else f""
     order = "ORDER BY created_at DESC"
     flag = 1 if id.id>0 else 0
     result = await db_select(select, schema, where, order, flag)
@@ -1014,8 +1014,8 @@ async def gettcbypo(id:GetPo):
     print(id.id)
     res_dt = {}
     select = "*"
-    schema = "td_mdcc,(SELECT @a:= 0) AS a"
-    where = f"po_no='{id.id}'" if id.id>0 else f""
+    schema = "td_mdcc"
+    where = f"sl_no='{id.id}'" if id.id>0 else f""
     order = "ORDER BY created_at DESC"
     flag = 1 if id.id>0 else 0
     result = await db_select(select, schema, where, order, flag)
@@ -1028,7 +1028,7 @@ async def gettcbypo(id:GetPo):
     print(id.id)
     res_dt = {}
     select = "*"
-    schema = "test_cert_doc"
+    schema = "test_cert_doc,(SELECT @a:= 0) AS a"
     where = f"test_cert_no='{id.id}'" if id.id>0 else f""
     order = "ORDER BY created_at DESC"
     flag = 0 if id.id>0 else 1
@@ -1042,7 +1042,7 @@ async def gettcbypo(id:GetPo):
     print(id.id)
     res_dt = {}
     select = "*"
-    schema = "td_mdcc_doc"
+    schema = "td_mdcc_doc,(SELECT @a:= 0) AS a"
     where = f"mdcc_no='{id.id}'" if id.id>0 else f""
     order = "ORDER BY created_at DESC"
     flag = 0 if id.id>0 else 1
