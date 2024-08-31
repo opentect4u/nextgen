b0eec40179515eb3ece8ebfc55ff8ab25415d5cc
somnathsynergic <somnath@synergicsoftek.in>
2024-08-31 11:33:39 +0530

    commit with docs

--- api/po.py
+++ api/po.py
@@ -1216,7 +1216,7 @@ async def adddelivery(data:getDelivery):
     return res_dt
 
 @poRouter.post('/add_delivery_files')
-async def add_proj_files(po_no:str = Form(...),docs1:str = Form(...), user:str = Form(...),docs2:Optional[Union[UploadFile, None]] = None):
+async def add_proj_files(po_no:str = Form(...),docs1:Optional[Union[UploadFile, None]] = None, user:str = Form(...),docs2:Optional[Union[UploadFile, None]] = None):
     fileName = ''
     res_dt = {}
     files = []
