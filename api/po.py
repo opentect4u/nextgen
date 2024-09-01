e381458a2830dc54b47d508331015cad424383d6
somnathsynergic <somnath@synergicsoftek.in>
2024-09-01 23:14:26 +0530

    commit with insert update

--- api/po.py
+++ api/po.py
@@ -1203,7 +1203,7 @@ async def adddelivery(data:getDelivery):
         fields= f'item_id="{v.sl_no}",quantity="{v.quantity}",status="{v.status}",modified_by="{data.user}",modified_at="{formatted_dt}"' if data.id > 0 else f'del_no,po_no,item_id,quantity,status,created_by,created_at'
         values = f'"{lastID}","{data.po_no}","{v.sl_no}","{v.quantity}","{v.status}","{data.user}","{formatted_dt}"'
         table_name = "td_item_delivery_details"
-        whr =  f'po_no="{data.po_no}"' if data.id > 0 else None
+        whr =  f'item_id="{v.sl_no}"' if data.id > 0 else None
         flag1 = 1 if data.id>0 else 0
 
         result = await db_Insert(table_name, fields, values, whr, flag1)
