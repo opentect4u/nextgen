cb8d3d4484f4e3960d37f9e81d334ef9943b2269
somnathsynergic <somnath@synergicsoftek.in>
2024-09-02 12:35:15 +0530

    commit with quantity_del

--- api/po.py
+++ api/po.py
@@ -1386,7 +1386,7 @@ async def getprojectpoc(id:GetPo):
     # print(id.id)
     res_dt = {}
 
-    select = "i.sl_no,i.po_sl_no,i.item_id,i.quantity,i.item_rt,i.discount_percent,i.delivery_dt,i.discount,p.prod_name"
+    select = "i.sl_no,i.po_sl_no,i.item_id,i.quantity,i.quantity_del,i.item_rt,i.discount_percent,i.delivery_dt,i.discount,p.prod_name"
     schema = "td_po_items i,md_product p"
     where = f"i.po_sl_no='{id.id}' and i.item_id=p.sl_no" if id.id>0 else ""
     order = ""
