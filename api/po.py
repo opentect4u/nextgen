04b45dc4619e1f8d5c6c26814c59d80d452a26ce
somnathsynergic <somnath@synergicsoftek.in>
2024-09-02 10:46:25 +0530

    commit with delete

--- api/po.py
+++ api/po.py
@@ -1379,3 +1379,18 @@ async def deletetc(id:deleteDoc):
    whr=f'sl_no="{id.id}"'
    result = await db_Insert(table_name, fields, values, whr, flag)
    return result
+
+
+@poRouter.post('/getpoitemfordel')
+async def getprojectpoc(id:GetPo):
+    # print(id.id)
+    res_dt = {}
+
+    select = "i.sl_no,i.po_sl_no,i.item_id,i.quantity,i.item_rt,i.discount_percent,i.delivery_dt,i.discount,p.prod_name"
+    schema = "td_po_items i,md_product p"
+    where = f"i.po_sl_no='{id.id}' and i.item_id=p.sl_no" if id.id>0 else ""
+    order = ""
+    flag = 1 if id.id>0 else 0
+    result = await db_select(select, schema, where, order, flag)
+    # print(result, 'RESULT')
+    return result
