c2caa91166ff4d941c61a9ea9465f1765ca0c2a2
somnathsynergic <somnath@synergicsoftek.in>
2024-09-02 18:36:08 +0530

    commit with delete

--- api/po.py
+++ api/po.py
@@ -343,7 +343,7 @@ async def getprojectpoc(id:GetPo):
 left join td_project p ON p.sl_no=b.project_id
 join md_vendor v ON v.sl_no=b.vendor_id
 join (SELECT @a:= 0) AS a '''
-    where = f"b.sl_no='{id.id}' and b.po_status IN ('P','U','A')" if id.id>0 else "b.po_status IN ('P','U','A') OR (amend_flag = 'Y' AND parent_po_no IS NOT NULL)"
+    where = f"b.sl_no='{id.id}' and b.po_status IN ('P','U','A','L','D')" if id.id>0 else "b.po_status IN ('P','U','A','L','D') OR (amend_flag = 'Y' AND parent_po_no IS NOT NULL)"
     order = "ORDER BY b.created_at DESC"
     flag = 0 if id.id>0 else 1
     result = await db_select(select, schema, where, order, flag)
