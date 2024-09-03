dbdedc886dd11a7c3edf51600c96368d0c17d5bf
somnathsynergic <somnath@synergicsoftek.in>
2024-09-03 12:28:25 +0530

    commit with file upload

--- api/po.py
+++ api/po.py
@@ -960,7 +960,7 @@ async def uploadfileToLocal1(file):
     modified_filename = f"{receipt}_{file.filename}"
     res = ""
     try:
-        file_location = os.path.join(UPLOAD_FOLDER, modified_filename)
+        file_location = os.path.join(UPLOAD_FOLDER2, modified_filename)
         print(file_location)
         
         with open(file_location, "wb") as f:
@@ -1274,7 +1274,7 @@ async def uploadfileToLocal2(file):
     modified_filename = f"{receipt}_{file.filename}"
     res = ""
     try:
-        file_location = os.path.join(UPLOAD_FOLDER, modified_filename)
+        file_location = os.path.join(UPLOAD_FOLDER3, modified_filename)
         print(file_location)
         
         with open(file_location, "wb") as f:
