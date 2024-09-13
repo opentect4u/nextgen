8c0fae93a9539ab29bef0dd46e817ad8d5a194a8
somnathsynergic <somnath@synergicsoftek.in>
2024-09-13 15:59:15 +0530

    commit with vendor type

--- api/po.py
+++ api/po.py
@@ -1766,9 +1766,9 @@ async def getreceiptdoc(wrd:GetPhrase):
     print('I am logging in!')
     # print(id.id)
     res_dt = {}
-    select = "distinct freight_insurance_val"
+    select = "distinct freight_ins_val"
     schema = "td_po_terms_condition"
-    where = f"freight_insurance_val like '%{wrd.wrd}%'"
+    where = f"freight_ins_val like '%{wrd.wrd}%'"
     order = "ORDER BY modified_by,created_at DESC"
     flag =  1
     result = await db_select(select, schema, where, order, flag)
