bbdc5c34530d0b69e527b0cd591b59c1733943f3
somnathsynergic <somnath@synergicsoftek.in>
2024-09-13 17:57:30 +0530

    commit with vendor type

--- api/po.py
+++ api/po.py
@@ -1830,5 +1830,19 @@ async def getreceiptdoc(wrd:GetPhrase):
     result = await db_select(select, schema, where, order, flag)
     print(result, 'RESULT')
     return result
+
+@poRouter.post('/get_manufacture_clearance')  
+async def getreceiptdoc(wrd:GetPhrase):
+    print('I am logging in!')
+    # print(id.id)
+    res_dt = {}
+    select = "distinct manufacture_clearance_desc"
+    schema = "td_po_terms_condition"
+    where = f"manufacture_clearance_desc like '%{wrd.wrd}%'"
+    order = "ORDER BY modified_by,created_at DESC"
+    flag =  1
+    result = await db_select(select, schema, where, order, flag)
+    print(result, 'RESULT')
+    return result
     
     
