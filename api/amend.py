1dc35304a13cbb6b69b77ace5ddf613b90b8b912
somnathsynergic <somnath@synergicsoftek.in>
2024-09-16 15:48:04 +0530

    commit with gst

--- api/amend.py
+++ api/amend.py
@@ -66,7 +66,7 @@ async def addpoamend(data:GetPo):
             table_name4 = "td_po_payment_dtls"
             result4 = await db_Insert(table_name4, fields4, None, None, 0, True)
 
-            fields5= f'SELECT NULL sl_no, "{lastID}" po_sl_no, price_basis, price_basis_desc, packing_fwd_extra, packing_fwd_extra_val, packing_fwd_val,pf_currency, pf_cgst,pf_sgst,pf_igst, freight_ins, freight_ins_val,ins, ins_val, test_certificate, test_certificate_desc,ld_date, ld_date_desc, ld_val, ld_val_desc, ld_val_per, min_per, warranty_guarantee, dispatch_dt,comm_dt, duration, duration_value, o_m_manual, o_m_desc, operation_installation, operation_installation_desc, packing_type,packing_val, manufacture_clearance, manufacture_clearance_desc,created_by,created_at, NULL modified_by, NULL modified_at FROM td_po_terms_condition WHERE po_sl_no = "{data.id}"'
+            fields5= f'SELECT NULL sl_no, "{lastID}" po_sl_no, price_basis, price_basis_desc, packing_fwd_extra, packing_fwd_extra_val, packing_fwd_val,pf_currency, pf_cgst,pf_sgst,pf_igst, freight_ins, freight_ins_val,freight_extra,freight_extra_val,freight_currency,freight_cgst,freight_sgst,freight_igst,ins, ins_val, test_certificate, test_certificate_desc,ld_date, ld_date_desc, ld_val, ld_val_desc, ld_val_per, min_per, warranty_guarantee, dispatch_dt,comm_dt, duration, duration_value, o_m_manual, o_m_desc, operation_installation, operation_installation_desc, packing_type,packing_val, manufacture_clearance, manufacture_clearance_desc,created_by,created_at, NULL modified_by, NULL modified_at FROM td_po_terms_condition WHERE po_sl_no = "{data.id}"'
             table_name5 = "td_po_terms_condition"
             result5 = await db_Insert(table_name5, fields5, None, None, 0, True)
     except:
