dbf5354cc12dc6ab3079e019ab2cfdbce444a53e
somnathsynergic <somnath@synergicsoftek.in>
2024-10-03 16:01:22 +0530

    commit with requisition

--- api/po.py
+++ api/po.py
@@ -2107,7 +2107,7 @@ async def get_requisition(data:GetReq):
     schema = "td_requisition_items,(SELECT @a:= 0) AS a"
     where = f"last_req_id='{data.Proj_id}' and delete_flag='N'" if data.Proj_id>0 else f"delete_flag='N'"
     order = "ORDER BY created_at DESC"
-    flag = 1
+    flag = 1  if data.Proj_id>0 else 1
     # if data.id>0 else 1
     result = await db_select(select, schema, where, order, flag)
     print(result, 'RESULT')
