7578213e462c6c16a2de88fb807d55e10dfb88c8
somnathsynergic <somnath@synergicsoftek.in>
2024-10-07 12:12:12 +0530

    commit with delivery

--- api/po.py
+++ api/po.py
@@ -1310,7 +1310,7 @@ async def adddelivery(data:getDelivery):
     
     select1 = "count(*) as count"
     schema1 = "td_item_delivery_invoice"
-    where1 = f"po_no='{data.po_no}'"
+    where1 = f"invoice='{data.invoice}'"
     order1 = ""
     flag1 = 0 
     result1 = await db_select(select1, schema1, where1, order1, flag1)
@@ -1322,7 +1322,7 @@ async def adddelivery(data:getDelivery):
     fields= f'po_no="{data.po_no}",ot_desc="{data.ot_desc}",invoice="{data.invoice}",invoice_dt="{data.invoice_dt}",lr_no="{data.lr_no}",waybill="{data.waybill}",ic="{data.ic}",og="{data.og}",dc="{data.dc}",lr="{data.lr}",wb="{data.wb}",pl="{data.pl}",om="{data.om}",om_manual="{data.om_manual}",ws="{data.ws}",tc="{data.tc}",wc="{data.wc}",ot="{data.ot}",confirm="{data.confirm}",modified_by="{data.user}",modified_at="{formatted_dt}"' if result1['msg']['count'] > 0 else f'mrn_no,po_no,ot_desc,invoice,invoice_dt,lr_no,waybill,ic,og,dc,lr,wb,pl,om,om_manual,ws,tc,wc,ot,confirm,created_by,created_at'
     values = f'"MRN-{data.invoice}","{data.po_no}","{data.ot_desc}","{data.invoice}","{data.invoice_dt}","{data.lr_no}","{data.waybill}","{data.ic}","{data.og}","{data.dc}","{data.lr}","{data.wb}","{data.pl}","{data.om}","{data.om_manual}","{data.ws}","{data.tc}","{data.wc}","{data.ot}","{data.confirm}","{data.user}","{formatted_dt}"'
     table_name = "td_item_delivery_invoice"
-    whr = f'po_no="{data.po_no}"' if result1['msg']['count'] > 0 else None
+    whr = f'invoice="{data.invoice}"' if result1['msg']['count'] > 0 else None
     flag = 1 if result1['msg']['count']>0 else 0
     result = await db_Insert(table_name, fields, values, whr, flag)
     lastID=result["lastId"]
