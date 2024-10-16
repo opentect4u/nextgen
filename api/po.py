37fede73c3ac8bf6bce24549c69e9f44c1b63f49
somnathsynergic <somnath@synergicsoftek.in>
2024-10-16 10:44:15 +0530

    commit with min

--- api/po.py
+++ api/po.py
@@ -2237,6 +2237,23 @@ async def item_dtls(data:req_id):
     res_dt = await db_select(select,table,where,order,flag)
     return res_dt
 
+@poRouter.post('/approve_req')
+async def approvepo(id:approvePO):
+    current_datetime = datetime.now()
+    formatted_dt = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+    fields= f'approval_flag="{id.status}",modified_by="{id.user}",modified_at="{formatted_dt}"'
+    values = f''
+    table_name = "td_requisition"
+    whr = f'sl_no="{id.id}"' if id.id > 0 else None
+    flag = 1 if id.id>0 else 0
+
+    result = await db_Insert(table_name, fields, values, whr, flag)
+    if result['suc']:
+        res_dt = {"suc": 1, "msg": f"Action Successful!"}
+    else:
+        res_dt = {"suc": 0, "msg": f"Error while saving!"}
+  
+    return res_dt
 
 
 
