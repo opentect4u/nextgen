4bae1bf48d0b615a6c66463c1da552f59b227134
somnathsynergic <somnath@synergicsoftek.in>
2024-10-24 14:51:53 +0530

    commit with min

--- api/po.py
+++ api/po.py
@@ -2070,7 +2070,7 @@ async def getreceiptdoc(wrd:GetPhrase):
     select = "distinct mdcc_scope"
     schema = "td_po_more"
     where = f"mdcc_scope like '%{wrd.wrd}%'"
-    order = "ORDER BY modified_at,created_at DESC"
+    order = "ORDER BY modified_dt,created_at DESC"
     flag =  1
     result = await db_select(select, schema, where, order, flag)
     print(result, 'RESULT')
@@ -2084,7 +2084,7 @@ async def getreceiptdoc(wrd:GetPhrase):
     select = "distinct draw_scope"
     schema = "td_po_more"
     where = f"draw_scope like '%{wrd.wrd}%'"
-    order = "ORDER BY modified_at,created_at DESC"
+    order = "ORDER BY modified_dt,created_at DESC"
     flag =  1
     result = await db_select(select, schema, where, order, flag)
     print(result, 'RESULT')
@@ -2098,7 +2098,7 @@ async def getreceiptdoc(wrd:GetPhrase):
     select = "distinct inspection_scope"
     schema = "td_po_more"
     where = f"inspection_scope like '%{wrd.wrd}%'"
-    order = "ORDER BY modified_at,created_at DESC"
+    order = "ORDER BY modified_dt,created_at DESC"
     flag =  1
     result = await db_select(select, schema, where, order, flag)
     print(result, 'RESULT')
