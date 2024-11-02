5201a42fd61b91e7c70f79c439b1ef7137f27df1
somnathsynergic <somnath@synergicsoftek.in>
2024-11-02 11:12:28 +0530

    commit with reason

--- api/po.py
+++ api/po.py
@@ -2738,7 +2738,7 @@ async def getprojectpoc(id:PoSearch):
     # print(id.id)
     res_dt = {}
 
-    select = "b.po_no,b.sl_no,b.fresh_flag,,b.po_status,b.amend_flag,b.project_id,b.vendor_id,v.vendor_name,d.proj_name,b.po_issue_date,i.item_id,p.prod_name,p.prod_make,p.part_no,b.created_by,b.created_at"
+    select = "b.po_no,b.sl_no,b.fresh_flag,b.po_status,b.amend_flag,b.project_id,b.vendor_id,v.vendor_name,d.proj_name,b.po_issue_date,i.item_id,p.prod_name,p.prod_make,p.part_no,b.created_by,b.created_at"
     schema = '''td_po_basic b left join td_po_items i on b.sl_no = i.po_sl_no
 left join md_product p ON p.sl_no=i.item_id left join md_vendor v on b.vendor_id=v.sl_no left join td_project d on b.project_id = d.sl_no
 '''
