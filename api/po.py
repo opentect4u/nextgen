a6434d1c772d6be1f03be8e04c3f527d1191d98d
somnathsynergic <somnath@synergicsoftek.in>
2024-11-04 12:36:57 +0530

    commit with search

--- api/po.py
+++ api/po.py
@@ -2775,7 +2775,7 @@ async def getprojectpoc(id:PoSearch):
     select = "d.invoice,d.invoice_dt,d.po_no,pr.sl_no,pr.proj_name,b.vendor_id,v.vendor_name,i.prod_id,p.prod_name,p.prod_make,p.part_no"
     schema = '''td_po_basic b left join td_item_delivery_invoice d on d.po_no = b.po_no left join td_project pr on pr.sl_no = b.project_id left join td_item_delivery_details i on i.po_no=d.po_no left join md_product p on i.prod_id = p.sl_no left join md_vendor v on v.sl_no=b.vendor_id
 '''
-    where = f"b.project_id='{id.project_id}' or b.vendor_id='{id.vendor_id}' or p.part_no like '%{id.part_no}%' or i.item_id='{id.prod_id}' or d.invoice_dt>={id.from_dt} and d.invoice_dt<={id.to_dt}"
+    where = f"b.project_id='{id.project_id}' or b.vendor_id='{id.vendor_id}' or p.part_no like '%{id.part_no}%' or i.prod_id='{id.prod_id}' or d.invoice_dt>={id.from_dt} and d.invoice_dt<={id.to_dt}"
     order = "ORDER BY d.created_at DESC"
     flag = 1
     result = await db_select(select, schema, where, order, flag)
