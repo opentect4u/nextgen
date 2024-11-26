2168110f832880de2678279d5645642ef90f3cd5
somnathsynergic <somnath@synergicsoftek.in>
2024-11-26 15:37:32 +0530

    commit with stock

--- api/report.py
+++ api/report.py
@@ -53,7 +53,7 @@ async def getprojectpoc(id:Itemwise):
     res_dt = {}
 
     select = f"SUM(st.qty*st.in_out_flag) stock,st.item_id,p.prod_name,st.proj_id"
-    schema = "td_stock_new st, md_product p,"
+    schema = "td_stock_new st, md_product p"
     where = f"st.item_id ={id.item_id} and st.item_id=p.sl_no and {id.dt}<=st.date group by st.proj_id"
     order = ""
     flag = 1 
