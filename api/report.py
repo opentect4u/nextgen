9ab2bde26c6b752f87e8d6edcf5ec406dfa6f70d
somnathsynergic <somnath@synergicsoftek.in>
2024-12-09 11:21:00 +0530

    commit with report

--- api/report.py
+++ api/report.py
@@ -60,7 +60,7 @@ async def getprojectpoc(id:Itemwise):
 async def getprojectpoc(id:AllItemwise):
     res_dt = {}
 
-    select = f"@a:=@a+1 serial_number,SUM(st.qty*st.in_out_flag)  project_stock,(SELECT SUM(qty*in_out_flag) project_stock FROM td_stock_new group by item_id having proj_id = 0) as warehouse_stock,st.item_id,p.prod_name,st.proj_id,pr.proj_name "
+    select = f"@a:=@a+1 serial_number,SUM(st.qty*st.in_out_flag)  project_stock,(SELECT SUM(qty*in_out_flag) project_stock FROM td_stock_new where proj_id = 0) as warehouse_stock,st.item_id,p.prod_name,st.proj_id,pr.proj_name "
     schema = "td_stock_new st, md_product p,td_project pr,(SELECT @a:= 0) AS a"
     where = f"pr.sl_no=st.proj_id and st.item_id=p.sl_no and '{id.dt}'>=st.date group by st.proj_id"
     order = ""
