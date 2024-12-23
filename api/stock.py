ef3c5981cf0d783c6a9ed5c685e53f62e92e5af8
somnathsynergic <somnath@synergicsoftek.in>
2024-12-23 14:26:12 +0530

    commit with transfer

--- api/stock.py
+++ api/stock.py
@@ -179,11 +179,11 @@ async def save_trans(data:GetTrans):
     result1= await db_select(select1, schema1, where1, order1, flag1)
     print('client_id=',result1['msg']['client_id'])
     res_dt = {}
-    select = f"t.trans_no,t.trans_dt,t.created_by,t.created_at,t.sl_no,p.proj_name,t.to_proj_id,t.purpose,t.intended_for,t.client_id,c.client_name"
-    schema = "td_transfer t,td_project p,md_client c"
+    select = f"t.trans_no,t.trans_dt,t.created_by,t.created_at,t.sl_no,p.proj_name,t.to_proj_id,t.purpose,t.intended_for,t.client_id,c.client_name" if result1['msg']['client_id']>0 else f"t.trans_no,t.trans_dt,t.created_by,t.created_at,t.sl_no,p.proj_name,t.to_proj_id,t.purpose,t.intended_for"
+    schema = f"td_transfer t,td_project p,md_client c" if result1['msg']['client_id']>0 else "td_transfer t,td_project p"
     where = f"t.sl_no='{data.id}' and t.trans_no like '%{'TWP-'}%' and p.sl_no=t.to_proj_id and c.sl_no = t.client_id" if result1['msg']['client_id']>0 else f"t.sl_no='{data.id}' and t.trans_no like '%{'TWP-'}%' and p.sl_no=t.to_proj_id"
     order = "ORDER BY t.created_at DESC"
-    flag = 0 if data.id>0 else 1
+    flag = 0 
     result = await db_select(select, schema, where, order, flag)
     print(result, 'RESULT')
     return result
