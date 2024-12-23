0f4fd7fc97569812caafb53d811f08d48808dec5
somnathsynergic <somnath@synergicsoftek.in>
2024-12-23 19:02:35 +0530

    commit with transfer

--- api/stock.py
+++ api/stock.py
@@ -289,32 +289,17 @@ async def save_trans(data:GetTrans):
 
 @stockRouter.post("/get_transfer_stock")
 async def save_trans(data:GetTrans):
-    # res_dt = {}
-
-    # select = "t.trans_no,t.trans_dt,t.created_by,t.created_at,t.sl_no,p.proj_name"
-    # schema = "td_transfer t,td_project p"
-    # where = f"t.sl_no='{data.id}' and p.sl_no=t.to_proj_id" if data.id>0 else f"p.sl_no=t.to_proj_id"
-    # order = "ORDER BY t.created_at DESC"
-    # flag = 0 if data.id>0 else 1
-    # result = await db_select(select, schema, where, order, flag)
-    # print(result, 'RESULT')
-    # return result
-
-    select1 = f"client_id"
-    schema1 = "td_transfer"
-    where1= f"sl_no='{data.id}'" 
-    order1 = "ORDER BY created_at DESC"
-    flag1 = 0 
-    result1= await db_select(select1, schema1, where1, order1, flag1)
     res_dt = {}
-    select = f"t.trans_no,t.trans_dt,t.created_by,t.created_at,t.from_proj_id,t.sl_no,p.proj_name,t.to_proj_id,t.purpose,t.intended_for,t.client_id,c.client_name,(select proj_name from td_project where sl_no=t.from_proj_id) as from_proj_name" if result1['msg']['client_id']>0 else f"t.trans_no,t.trans_dt,t.created_by,t.created_at,t.sl_no,p.proj_name,t.to_proj_id,t.purpose,t.intended_for,(select proj_name from td_project where sl_no=t.from_proj_id) as from_proj_name"
-    schema = f"td_transfer t,td_project p,md_client c" if result1['msg']['client_id']>0 else "td_transfer t,td_project p"
-    where = f"p.sl_no=t.to_proj_id and c.sl_no = t.client_id" if result1['msg']['client_id']>0 else f" p.sl_no=t.to_proj_id"
+
+    select = "t.trans_no,t.trans_dt,t.created_by,t.created_at,t.sl_no,p.proj_name"
+    schema = "td_transfer t,td_project p"
+    where = f"t.sl_no='{data.id}' and p.sl_no=t.to_proj_id" if data.id>0 else f"p.sl_no=t.to_proj_id"
     order = "ORDER BY t.created_at DESC"
-    flag =  1
+    flag = 0 if data.id>0 else 1
     result = await db_select(select, schema, where, order, flag)
     print(result, 'RESULT')
     return result
+
 
 
 
