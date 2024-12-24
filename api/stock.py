0bc64f70fe21fd018ffbd48f247f5f0fd6cd2fd1
somnathsynergic <somnath@synergicsoftek.in>
2024-12-24 17:20:50 +0530

    commit with transfer approval

--- api/stock.py
+++ api/stock.py
@@ -345,7 +345,7 @@ async def getprojectpoc(id:GetStock):
     order1 = ""
     flag1 = 0 
     result1 = await db_select(select1, schema1, where1, order1, flag1)
-    print("qty=======",result1)
+    print("qty=======",result1['msg']['req_stock'])
 
 
     select = f"SUM(qty*in_out_flag) project_stock, (SELECT SUM(qty*in_out_flag) project_stock FROM td_stock_new where item_id={id.prod_id} and proj_id = 0) as warehouse_stock, sum(req_qty*in_out_flag) req_qty"
