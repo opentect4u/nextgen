e0af799e599bf338b875fa1e552de1de6c7bc9e4
somnathsynergic <somnath@synergicsoftek.in>
2025-01-07 17:33:31 +0530

    commit with superadmin

--- api/stock.py
+++ api/stock.py
@@ -367,6 +367,28 @@ async def getprojectpoc(id:GetStock):
     # print(result, 'RESULT')
     return {"result":result,"req_stock":result1['msg']['req_stock']}
 
+@stockRouter.post('/get_logical_stock_req')
+async def getprojectpoc(id:GetStock):
+    # print(id.id)
+    res_dt = {}
+    select1 = f"sum(i.qty) as req_stock"
+    schema1 = "td_requisition t,td_requisition_items i"
+    where1 = f"i.item_id={id.prod_id} and t.project_id ={id.proj_id} and i.approve_flag='P' and i.req_no=t.req_no"
+    order1 = ""
+    flag1 = 0 
+    result1 = await db_select(select1, schema1, where1, order1, flag1)
+    print("qty=======",result1['msg']['req_stock'])
+
+    # select = f"SUM(qty*in_out_flag) project_stock, (SELECT SUM(qty*in_out_flag) project_stock FROM td_stock_new where item_id={id.prod_id} and proj_id = 0) as warehouse_stock, sum(req_qty*in_out_flag) req_qty"
+    select = f"(SELECT SUM(qty*in_out_flag)  td_stock_new where item_id={id.prod_id} and proj_id = {id.proj_id} and proj_id!=0) project_stock, (SELECT SUM(qty*in_out_flag) project_stock FROM td_stock_new where item_id={id.prod_id} and proj_id = 0) as warehouse_stock, sum(req_qty*in_out_flag) req_qty"
+    schema = "td_stock_new"
+    where = f"item_id={id.prod_id} and proj_id ={id.proj_id}"
+    order = ""
+    flag = 1 
+    result = await db_select(select, schema, where, order, flag)
+    # print(result, 'RESULT')
+    return {"result":result,"req_stock":result1['msg']['req_stock']}
+
 @stockRouter.post("/approve_transfer_items")
 async def save_trans(data:GetApproveItems):
     current_datetime = datetime.now()
