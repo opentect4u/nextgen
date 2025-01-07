bdc9f6ae5aa2f0805e71d37f5b2efa62989ea676
somnathsynergic <somnath@synergicsoftek.in>
2025-01-07 17:59:24 +0530

    commit with superadmin

--- api/stock.py
+++ api/stock.py
@@ -377,10 +377,9 @@ async def getprojectpoc(id:GetStock):
     order1 = ""
     flag1 = 0 
     result1 = await db_select(select1, schema1, where1, order1, flag1)
-    print("qty=======",result1['msg']['req_stock'])
+    # print("qty=======",result1['msg']['req_stock'])
 
-    # select = f"SUM(qty*in_out_flag) project_stock, (SELECT SUM(qty*in_out_flag) project_stock FROM td_stock_new where item_id={id.prod_id} and proj_id = 0) as warehouse_stock, sum(req_qty*in_out_flag) req_qty"
-    select = f"(SELECT SUM(qty*in_out_flag)  td_stock_new where item_id={id.prod_id} and proj_id = {id.proj_id} and proj_id!=0) project_stock, (SELECT SUM(qty*in_out_flag) project_stock FROM td_stock_new where item_id={id.prod_id} and proj_id = 0) as warehouse_stock, sum(req_qty*in_out_flag) req_qty"
+    select = f"(SELECT SUM(qty*in_out_flag) td_stock_new where item_id={id.prod_id} and proj_id = {id.proj_id} and proj_id!=0) project_stock, (SELECT SUM(qty*in_out_flag) project_stock FROM td_stock_new where item_id={id.prod_id} and proj_id = 0) as warehouse_stock, sum(req_qty*in_out_flag) req_qty"
     schema = "td_stock_new"
     where = f"item_id={id.prod_id} and proj_id ={id.proj_id}"
     order = ""
