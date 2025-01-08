2e7a8e888991f64e15c0b4ba862ed085272f7f36
somnathsynergic <somnath@synergicsoftek.in>
2025-01-08 11:55:45 +0530

    commit with superadmin

--- api/report.py
+++ api/report.py
@@ -72,8 +72,8 @@ async def getprojectpoc(id:Itemwise):
 
 
 
-    select = f"@a:=@a+1 serial_number,SUM(st.qty*st.in_out_flag) project_stock,SUM(i.req_qty) req_stock,(SELECT SUM(qty*in_out_flag) project_stock FROM td_stock_new where item_id={id.item_id} and proj_id = 0) as warehouse_stock,st.item_id,p.prod_name,st.proj_id,pr.proj_name,td_requisition_items i"
-    schema = "td_stock_new st, md_product p,td_project pr,(SELECT @a:= 0) AS a"
+    select = f"@a:=@a+1 serial_number,SUM(st.qty*st.in_out_flag) project_stock,SUM(i.req_qty) req_stock,(SELECT SUM(qty*in_out_flag) project_stock FROM td_stock_new where item_id={id.item_id} and proj_id = 0) as warehouse_stock,st.item_id,p.prod_name,st.proj_id,pr.proj_name"
+    schema = "td_stock_new st, md_product p,td_project pr,td_requisition_items i,(SELECT @a:= 0) AS a"
     where = f"st.item_id ={id.item_id} and pr.sl_no=st.proj_id and st.item_id=p.sl_no and '{id.dt}'>=st.date group by st.proj_id and i.approve_flag='P'"
     order = ""
     flag = 1 
