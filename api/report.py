ebec27d1149335dc1d8069ef5177a59d00b92e9e
somnathsynergic <somnath@synergicsoftek.in>
2025-01-08 11:14:44 +0530

    commit with superadmin

--- api/report.py
+++ api/report.py
@@ -59,14 +59,14 @@ async def getprojectpoc(id:Itemwise):
 @reportRouter.post('/itemwisestockin')
 async def getprojectpoc(id:Itemwise):
     res_dt = {}
-    select1 = f"sum(i.req_qty) as req_stock"
-    schema1 = "td_requisition t,td_requisition_items i"
-    where1 = f"i.item_id={id.item_id} and i.approve_flag='P' and i.req_no=t.req_no"
+    select1 = f"sum(req_qty) as req_stock"
+    schema1 = "td_requisition_items"
+    where1 = f"item_id={id.item_id} and approve_flag='P'"
     order1 = ""
     flag1 = 0 
     result1 = await db_select(select1, schema1, where1, order1, flag1)
     print(result1)
-    select = f"@a:=@a+1 serial_number,SUM(st.qty*st.in_out_flag)  project_stock,SUM(st.req_qty*st.in_out_flag)  req_stock,(SELECT SUM(qty*in_out_flag) project_stock FROM td_stock_new where item_id={id.item_id} and proj_id = 0) as warehouse_stock,st.item_id,p.prod_name,st.proj_id,pr.proj_name "
+    select = f"@a:=@a+1 serial_number,SUM(st.qty*st.in_out_flag) project_stock,SUM(st.req_qty*st.in_out_flag)  req_stock,(SELECT SUM(qty*in_out_flag) project_stock FROM td_stock_new where item_id={id.item_id} and proj_id = 0) as warehouse_stock,st.item_id,p.prod_name,st.proj_id,pr.proj_name "
     schema = "td_stock_new st, md_product p,td_project pr,(SELECT @a:= 0) AS a"
     where = f"st.item_id ={id.item_id} and pr.sl_no=st.proj_id and st.item_id=p.sl_no and '{id.dt}'>=st.date group by st.proj_id"
     order = ""
