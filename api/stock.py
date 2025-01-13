63b57999c7b199eb5a8dd535c91cb18b9da97c3c
somnathsynergic <somnath@synergicsoftek.in>
2025-01-13 16:31:01 +0530

    commit with superadmin

--- api/stock.py
+++ api/stock.py
@@ -411,7 +411,7 @@ async def getprojectpoc(id:GetStock):
     flag =1 
     result = await db_select(select, schema, where, order, flag)
     if result1['suc']>0:
-       return {"result":result,"req_stock":result1['msg']['req_stock'],"tot_stock":result_tot['msg']['tot_stock'] - result2['msg']['del_stock'] if result_tot['msg']['tot_stock'] and result2['msg']['tot_stock'] else result_tot['msg']['tot_stock'] if result['msg']['tot_stock'] else 0}
+       return {"result":result,"req_stock":result1['msg']['req_stock'],"tot_stock":result_tot['msg']['tot_stock'] - result2['msg']['del_stock'] if result_tot['msg']['tot_stock'] and result_tot['msg']['tot_stock'] else result_tot['msg']['tot_stock'] if result_tot['msg']['tot_stock'] else 0}
     else:
        return {"result":result,"req_stock":0,"tot_stock":0}
     
