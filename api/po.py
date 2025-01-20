b02258978c2eab98975f5a9b4789a997ca5b18d0
somnathsynergic <somnath@synergicsoftek.in>
2025-01-20 13:50:34 +0530

    commit with req_min

--- api/po.py
+++ api/po.py
@@ -582,9 +582,6 @@ left join td_project p ON p.sl_no=b.project_id
 join md_vendor v ON v.sl_no=b.vendor_id 
 join (SELECT @a:= 0) AS a 
 JOIN (
-    SELECT po_no
-    FROM td_po_basic WHERE amend_flag = 'N' AND po_no is NOT null
-    UNION
     SELECT MAX(po_no) po_no
     FROM td_po_basic
     WHERE amend_flag = 'Y' AND po_no is NOT null
