c74a037c2cc0fa6b7a021909da22d6dce2c91795
somnathsynergic <somnath@synergicsoftek.in>
2025-01-22 18:44:03 +0530

    commit with save_pur_req

--- api/stock.py
+++ api/stock.py
@@ -691,7 +691,7 @@ async def save_trans(data:GetTrans):
 
     select = "t.pur_no,t.pur_date,t.created_by,t.created_at,t.sl_no,p.proj_name"
     schema = "td_purchase_req t,td_project p"
-    where = f"t.sl_no='{data.id}' and p.sl_no=t.to_proj_id" if data.id>0 else f"p.sl_no=t.to_proj_id"
+    where = f"t.sl_no='{data.id}' and p.sl_no=t.pur_proj" if data.id>0 else f"p.sl_no=t.pur_proj"
     order = "ORDER BY t.created_at DESC"
     flag = 0 if data.id>0 else 1
     result = await db_select(select, schema, where, order, flag)
