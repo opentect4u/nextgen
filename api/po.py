58bdd6fca100564ca52aededd719d5d2b6e95098
somnathsynergic <somnath@synergicsoftek.in>
2025-02-10 16:45:34 +0530

    commit with return

--- api/po.py
+++ api/po.py
@@ -3648,7 +3648,7 @@ async def getMinReq(dt:GetStock):
 
     res_dt = {}
     
-    select = f"distinct req.sl_no,req.req_date,req.req_no,r.item_id,sum(r.approved_qty) as approved_qty,sum(r.cancelled_qty) as cancelled_qty,sum(st.qty) as stock_out_qty,(select sum(qty) from td_return_items where proj_id={dt.proj_id} and item_id={dt.prod_id} group by ref_no) as return_qty"
+    select = f"distinct req.sl_no,req.req_date,req.req_no,r.item_id,sum(r.approved_qty) as approved_qty,sum(r.cancelled_qty) as cancelled_qty,sum(st.qty) as stock_out_qty,(select sum(qty) from td_return_items where proj_id={dt.proj_id} and item_id={dt.prod_id} and ref_no like %r.req_no group by ref_no) as return_qty"
     schema = "td_requisition_items r left join td_requisition req on req.req_no = r.req_no left join td_stock_new st on req.req_no = st.ref_no "
     where = f"req.project_id={dt.proj_id} and r.item_id={dt.prod_id} and req.req_no=r.req_no group by st.ref_no"
     order = ""
