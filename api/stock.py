311f3c8c3422b8aaf13c1bbd4b233239648d062f
somnathsynergic <somnath@synergicsoftek.in>
2025-03-04 18:23:54 +0530

    commit with stock

--- api/stock.py
+++ api/stock.py
@@ -278,7 +278,7 @@ async def save_trans(data:GetTrans):
 
     select = "t.trans_no,t.trans_dt,t.created_by,t.created_at,t.sl_no,p.proj_name"
     schema = "td_transfer t,td_project p"
-    where = f"t.sl_no='{data.id}' and t.trans_no like '%{'TPW-'}%' and p.sl_no=t.to_proj_id" if data.id>0 else f"t.trans_no like '%{'TPW-'}%' and p.sl_no=t.to_proj_id"
+    where = f"t.sl_no='{data.id}' and t.trans_no like '%{'TPW-'}%' and p.sl_no=t.from_proj_id" if data.id>0 else f"t.trans_no like '%{'TPW-'}%' and p.sl_no=t.from_proj_id"
     order = "ORDER BY t.created_at DESC"
     flag = 0 if data.id>0 else 1
     result = await db_select(select, schema, where, order, flag)
