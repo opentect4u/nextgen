0d2d544375c8d35c6967a8a8745d7c7c980b762b
somnathsynergic <somnath@synergicsoftek.in>
2025-03-07 10:48:13 +0530

    commit with stock

--- api/master.py
+++ api/master.py
@@ -1400,8 +1400,8 @@ async def check_product(wrd:getPhrase):
     
     select_prod = "p.prod_name,c.catg_name,p.part_no "
     schema_prod = "md_product p,md_category c"
-    where_prod = f"p.prod_cat=c.sl_no limit 4"
-    order_prod = "order by p.created_by desc"
+    where_prod = f"p.prod_cat=c.sl_no "
+    order_prod = "order by p.created_by desc limit 4"
     flag_prod = 1 
     result_prod = await db_select(select_prod, schema_prod, where_prod, order_prod, flag_prod)
 
