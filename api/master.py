7421c60e725f7168e1acf87a341ce1f0d55db199
somnathsynergic <somnath@synergicsoftek.in>
2025-03-10 09:57:23 +0530

    commit with filename

--- api/master.py
+++ api/master.py
@@ -765,8 +765,9 @@ async def deleteuser(id:deleteData):
 @masterRouter.post('/addclient')
 # poc_doc: Optional[List[UploadFile]] = File(None)
 # poc_doc:List[UploadFile] = File(...)
-async def addclient(client_data:str = Form(...), poc_doc: Optional[List[UploadFile]] = File(None)):
+async def addclient(client_data:str = Form(...), poc_doc: Optional[List[UploadFile]] = File(...)):
     res_dt = {}
+    print('poc_doc_outside=',poc_doc)
     data = json.loads(client_data)
     # print(data['c_name'])
     # for file in poc_doc:
