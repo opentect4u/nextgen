7f93ffe12125e9e3be4a0bbe99f4dc29343b3263
somnathsynergic <somnath@synergicsoftek.in>
2025-03-10 12:34:03 +0530

    commit with filename

--- api/master.py
+++ api/master.py
@@ -852,7 +852,7 @@ async def addclient(request: Request,client_data:str = Form(...)):
             #         fileName = f"upload_poc/{fileName}"
              for file in files:
                 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-                file_path = os.path.join(UPLOAD_POC_FOLDER, f"{timestamp}_{file}")
+                file_path = os.path.join(UPLOAD_POC_FOLDER, f"{timestamp}_{file['filename']}")
                 await uploadfileToLocal(file, UPLOAD_POC_FOLDER)
                 print(f"File saved to: {file_path}")
         except Exception as e:
