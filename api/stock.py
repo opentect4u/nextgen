933c4c16125b6407fbe3ece3bf8ebc7affff0d61
somnathsynergic <somnath@synergicsoftek.in>
2025-03-12 13:56:26 +0530

    commit with pur_req_edit

--- api/stock.py
+++ api/stock.py
@@ -1072,8 +1072,8 @@ async def save_trans(data:GetTrans):
 
 @stockRouter.post("/get_purchase_req_items_for_po")
 async def save_trans(data:GetPurItemForPo):
-    # pur_no =  ",".join(str(dt) for dt in data)
-    pur_no =  ",".join(data)
+    pur_no =  ",".join(str(dt) for dt in data and str(dt).startswith("'PR-"))
+    # pur_no =  ",".join(data)
     print("pur_no=",data,pur_no)
     mrn_dt = ""
     select1 = "*"
