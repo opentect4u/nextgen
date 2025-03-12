994f5bd8a858b67ca3530443e919d014b8879c16
somnathsynergic <somnath@synergicsoftek.in>
2025-03-12 17:00:26 +0530

    commit with pur_req_edit

--- api/stock.py
+++ api/stock.py
@@ -1075,9 +1075,9 @@ async def save_trans(data:GetPurItemForPo):
     print("pur_nooooooooooooooooooooooooooooooooooooooooooo", str(data).split('=')[1].rstrip(','))
     dt = str(data).split('=')[1].rstrip(',').strip("'")
     pur_no= ','.join(f"'{item}'" for item in dt.split(','))
-    select1 = "*"
+    select1 = "sum(qty) as qty,item_id"
     schema1 = "td_purchase_items"
-    where1 = f"pur_no in ({pur_no})"
+    where1 = f"pur_no in ({pur_no}) group by item_id"
     order1 = ""
     flag1 =  1
     result1 = await db_select(select1, schema1, where1, order1, flag1)
