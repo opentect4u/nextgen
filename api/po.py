a3889e746d0ec25d9b7c1a6ffe781769fbc50581
somnathsynergic <somnath@synergicsoftek.in>
2025-03-15 14:42:18 +0530

    commit with po

--- api/po.py
+++ api/po.py
@@ -1072,6 +1072,7 @@ async def addfreshpo(data:PoModel):
                 ordered_qty = 0
                 
                 for pur_qty in result_pur['msg']:
+                    print('pur_qty============',pur_qty)
                     if pur_qty['item_id'] == c.sl_no:
                         if pur_qty['qty']<=sum_qty:
                             ordered_qty = pur_qty['qty']
