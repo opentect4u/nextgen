d0941d4ce23e28f9f27e11026bbe30a2463a3bb8
somnathsynergic <somnath@synergicsoftek.in>
2025-03-25 08:27:04 +0530

    commit with req

--- api/report.py
+++ api/report.py
@@ -121,7 +121,7 @@ async def getprojectpoc(id:GetStockOut):
         result_req = await db_select(select_req, schema_req, where_req, order_req, flag_req)
         print(result_req)
 
-        select_trans = f"i.approved_qty, i.approved_qty as copy_qty,r.trans_no as req_no,i.item_id,r.to_proj_id as proj_id,(select sum(qty) from td_stock_new st where st.item_id={i['item_id']} and st.proj_id={id.proj_id} and st.ref_no=r.trans_no) del_qty"
+        select_trans = f"i.approved_qty, i.approved_qty as copy_qty,r.trans_no as req_no,i.item_id,r.to_proj_id as proj_id,(select sum(qty) from td_stock_new st where st.item_id={i['item_id']} and st.proj_id={id.proj_id} and st.ref_no=r.trans_no and in_out_flag=-1) del_qty"
         schema_trans = "td_transfer_items i,td_transfer r"
         where_trans = f"i.item_id={i['item_id']} and r.to_proj_id={id.proj_id} and (i.approve_flag='A' || i.approve_flag='H') and i.trans_no=r.trans_no"
         order_trans = ""
