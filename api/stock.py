86bde319b315d418cc175436ae8585b2303fe5c1
somnathsynergic <somnath@synergicsoftek.in>
2025-03-25 15:37:22 +0530

    commit with req

--- api/stock.py
+++ api/stock.py
@@ -1153,9 +1153,9 @@ async def save_trans(data:GetPurItem):
     print('result1 ===================================',result2)
    
 
-    select = f"i.sl_no as item_sl,i.ordered_qty,i.pur_no,i.item_id,i.qty,i.created_by,i.created_at,i.status,i.modified_at,i.modified_by, sum(m.rc_qty) as tot_rc" 
-    schema = f"td_purchase_items i left join td_item_delivery_details m on i.item_id=m.prod_id" 
-    where = f"i.pur_no='{data.pur_no}' and m.po_no in (select po_no from td_po_basic where pur_req like '%{data.pur_no.lstrip('PR-')}%') " 
+    select = f"sl_no as item_sl,ordered_qty,pur_no,item_id,qty,created_by,created_at,status,modified_at,modified_by" 
+    schema = f"td_purchase_items" 
+    where = f"pur_no='{data.pur_no}'" 
     order = ""
     flag =  1
     result = await db_select(select, schema, where, order, flag)
