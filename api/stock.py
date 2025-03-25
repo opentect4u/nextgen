aaaa176d91983d39e99551991ee3291a7ade1bb3
somnathsynergic <somnath@synergicsoftek.in>
2025-03-25 15:48:06 +0530

    commit with req

--- api/stock.py
+++ api/stock.py
@@ -1160,6 +1160,11 @@ async def save_trans(data:GetPurItem):
     flag =  1
     result = await db_select(select, schema, where, order, flag)
     print(result, 'RESULT')
+    delivery_data = {row['prod_id']: row['total_received'] for row in result2}
+
+# Merge the total_received value into result based on item_id
+    for row in result:
+       row['tot_rc'] = delivery_data.get(row['item_id'], 0) 
     return result
 
     # select = f"p.sl_no as item_sl,p.ordered_qty,p.pur_no,p.item_id,p.qty,p.created_by,p.created_at,p.status,p.modified_at,p.modified_by,sum(m.qty) as tot_rc" 
