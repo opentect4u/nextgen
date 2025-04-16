2c59eba85f1d2ca6f3ea087e6e3e17b8cffa5b9c
somnathsynergic <somnath@synergicsoftek.in>
2025-04-16 17:02:37 +0530

    commit with purchase requisition

--- api/stock.py
+++ api/stock.py
@@ -1363,7 +1363,7 @@ async def save_trans(data:GetLog):
         result = await db_select(select, schema, where, order, flag)
         print(result, 'RESULT')
     else:
-        select = "po_no,del_no as mrn_no,item_id as prod_id,qty as rc_qty" 
+        select = "po_no,del_no as invooice,item_id as prod_id,qty as rc_qty" 
         schema = f"td_vtoc_items" 
         where =f"del_no in (select del_no from td_vtoc_items where po_no in  (select po_no from td_po_basic where pur_req like '%{data.pur_no.lstrip('PR-')}%' and po_status='A')) and item_id = {data.item_id}"
         order = ""
