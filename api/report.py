aeadae75552a36566ebc3a131ddf655e7572bb3d
somnathsynergic <somnath@synergicsoftek.in>
2025-04-19 11:22:39 +0530

    commit with vendor_name

--- api/report.py
+++ api/report.py
@@ -52,7 +52,7 @@ async def getprojectpoc(id:Itemwise):
 
     select = f"@a:=@a+1 serial_number,SUM(st.qty*st.in_out_flag)  project_stock,(SELECT SUM(qty*in_out_flag) FROM td_stock_new where item_id={id.item_id} and proj_id = 0) as warehouse_stock,st.item_id,p.prod_name,st.proj_id,pr.proj_name "
     schema = "td_stock_new st, md_product p,td_project pr,(SELECT @a:= 0) AS a"
-    where = f"st.item_id ={id.item_id} and pr.sl_no=st.proj_id and st.item_id=p.sl_no and '{id.dt}'>=st.date group by st.proj_id"
+    where = f"st.item_id ={id.item_id} and (pr.sl_no=st.proj_id or st.proj_id=0) and st.item_id=p.sl_no and '{id.dt}'>=st.date group by st.proj_id"
     order = ""
     flag = 1 
     result = await db_select(select, schema, where, order, flag)
