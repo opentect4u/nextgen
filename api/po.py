e62ffbb44b01dfe43cb9d6853adf268649a708d0
somnathsynergic <somnath@synergicsoftek.in>
2025-05-06 14:49:40 +0530

    commit with report

--- api/po.py
+++ api/po.py
@@ -139,6 +139,11 @@ class PoModel(BaseModel):
 
 class GetPo(BaseModel):
     id:int
+
+class GetPoForTc(BaseModel):
+    id:int
+    po_no:str
+
 class GetPur(BaseModel):
     id:str
 
@@ -742,13 +747,13 @@ async def getprojectpoc(id:GetPo):
     # print(result, 'RESULT')
     return result
 @poRouter.post('/getpoitemfortc')
-async def getprojectpoc(id:GetPo):
+async def getprojectpoc(id:GetPoForTc):
     # print(id.id)
     res_dt = {}
 
-    select = "i.sl_no,i.po_sl_no,i.item_id,i.quantity,i.item_rt,i.discount_percent,i.discount,p.prod_name"
-    schema = "td_po_items i,md_product p"
-    where = f"i.po_sl_no='{id.id}' and i.item_id=p.sl_no" if id.id>0 else ""
+    select = "i.sl_no,i.po_sl_no,i.item_id,i.quantity,i.item_rt,i.discount_percent,i.discount,p.prod_name,d.rc_qty"
+    schema = "td_po_items i,md_product p,td_item_delivery_details d"
+    where = f"i.po_sl_no='{id.id}' and i.item_id=p.sl_no and d.item_id=i.item_id and i.po_no='{id.po_no}'" if id.id>0 else ""
     order = ""
     flag = 1 if id.id>0 else 0
     result = await db_select(select, schema, where, order, flag)
