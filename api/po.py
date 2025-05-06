68a51a0989f63a94779b4f0a1af23c8d7082368e
somnathsynergic <somnath@synergicsoftek.in>
2025-05-06 15:25:40 +0530

    commit with report

--- api/po.py
+++ api/po.py
@@ -752,8 +752,8 @@ async def getprojectpoc(id:GetPoForTc):
     res_dt = {}
 
     select = "i.sl_no,i.po_sl_no,i.item_id,i.quantity,i.item_rt,i.discount_percent,i.discount,p.prod_name,d.rc_qty"
-    schema = "td_po_items i left join md_product p on i.item_id=p.sl_no left join td_item_delivery_details d on d.item_id=i.item_id"
-    where = f"i.po_sl_no='{id.id}' and d.po_no='{id.po_no}'" if id.id>0 else ""
+    schema = "td_po_items i left join md_product p on i.item_id=p.sl_no left join td_item_delivery_details d on i.item_id=d.item_id and d.po_no='{id.po_no}'"
+    where = f"i.po_sl_no='{id.id}' " if id.id>0 else ""
     order = ""
     flag = 1 if id.id>0 else 0
     result = await db_select(select, schema, where, order, flag)
