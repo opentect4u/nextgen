41e42840005079c9806ea60860e53f30b7fd6ca8
somnathsynergic <somnath@synergicsoftek.in>
2025-05-08 13:46:30 +0530

    commit with reports

--- api/report.py
+++ api/report.py
@@ -285,8 +285,8 @@ async def getprojectpoc(id:stockoutreport):
     res_dt = {}
 
     select = f"st.date,st.item_id,st.qty,st.created_by,p.prod_name,p.part_no,p.article_no,p.model_no,p.prod_desc,st.in_out_flag,st.ref_no,st.proj_id,pr.proj_name"
-    schema = "td_stock_new st, md_product p,td_project pr"
-    where = f"st.item_id=p.sl_no and st.proj_id=pr.sl_no" if id .type=='P' else f"st.item_id=p.sl_no and st.proj_id=0"
+    schema = "td_stock_new st left join md_product p on st.item_id=p.sl_no left join td_project pr on st.proj_id=pr.sl_no" if id.type=='P' else "td_stock_new st left join md_product p on st.item_id=p.sl_no and st.proj_id=0"
+   
     where = f"st.in_out_flag=-1 and ref_no like '%REQ%'"
     order = ""
     flag = 1 
