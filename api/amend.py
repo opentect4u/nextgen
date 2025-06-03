e7842215cb89d70c5e5922dea0521f8a3e242673
somnathsynergic <somnath@synergic.in>
2025-06-03 12:50:40 +0530

    commit with amendment

--- api/amend.py
+++ api/amend.py
@@ -104,7 +104,11 @@ async def addpoamend(data:GetPo):
     parent_po = await db_select("po_no","td_po_basic", f"sl_no = {data.id}","",0) 
     am_po_row = await db_select("ifnull(MAX(SUBSTRING_INDEX(po_no, '-', -1))+1,1) po_no","td_po_basic", f"po_no like '{parent_po['msg']['po_no']}-%'","",0)
     print(am_po_row['msg']['po_no'], '-----------------------')
-    am_po_no = f'{parent_po['msg']['po_no']}-{str(am_po_row['msg']['po_no']).split('.')[0]}' if amend_flag['msg']['amend_flag']=='N' else f'{str(parent_po['msg']['po_no']).rsplit('-')[0]}-{str(am_po_row['msg']['po_no']).split('.')[0]}'
+    if amend_flag=='Y':
+        txt = str(parent_po['msg']['po_no'])
+        x = txt.split("-")
+        joined_string = "-".join(x[:-1])
+    am_po_no = f'{parent_po['msg']['po_no']}-{str(am_po_row['msg']['po_no']).split('.')[0]}' if amend_flag['msg']['amend_flag']=='N' else f'{joined_string}-{str(am_po_row['msg']['po_no']).split('.')[0]}'
     # am_po_no = await db_select(f"CONCAT((SELECT po_no FROM td_po_basic WHERE sl_no = {data.id}), '-', IF(LENGTH(po_no)-LENGTH(replace(po_no,'-',''))> 1, MAX(SUBSTRING_INDEX(po_no, '-', -1))+1, 1)) am_po", "td_po_basic", f"SUBSTRING_INDEX(po_no, '-', 2) = (SELECT po_no FROM td_po_basic WHERE sl_no = {data.id})", "", 0)
 
     print('am_po_no',am_po_no)
