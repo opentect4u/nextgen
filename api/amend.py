e09ae303b6cc84ea52ec4cd363c495b8ba70701c
somnathsynergic <somnath@synergic.in>
2025-06-03 12:59:09 +0530

    commit with amendment

--- api/amend.py
+++ api/amend.py
@@ -108,7 +108,7 @@ async def addpoamend(data:GetPo):
         txt = str(parent_po['msg']['po_no'])
         x = txt.split("-")
         joined_string = "-".join(x[:-1])
-        print(joined_string)
+        print('joined string',joined_string)
     am_po_no = f'{parent_po['msg']['po_no']}-{str(am_po_row['msg']['po_no']).split('.')[0]}' if amend_flag['msg']['amend_flag']=='N' else f'{joined_string}-{str(am_po_row['msg']['po_no']).split('.')[0]}'
     # am_po_no = await db_select(f"CONCAT((SELECT po_no FROM td_po_basic WHERE sl_no = {data.id}), '-', IF(LENGTH(po_no)-LENGTH(replace(po_no,'-',''))> 1, MAX(SUBSTRING_INDEX(po_no, '-', -1))+1, 1)) am_po", "td_po_basic", f"SUBSTRING_INDEX(po_no, '-', 2) = (SELECT po_no FROM td_po_basic WHERE sl_no = {data.id})", "", 0)
 
