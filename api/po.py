efbc54872d0d4ec8d7754552af6f8c4384ee2866
somnathsynergic <somnath@synergic.in>
2025-06-04 18:42:36 +0530

    commit with siemens

--- api/po.py
+++ api/po.py
@@ -4826,9 +4826,9 @@ async def getParentPoDate(po_no:GetPurchaseMrn):
 async def getParentPoDate(items:SiemensInput):
     for c in items.items:
                 fields1= f'po_no,proj_id,prod_id,order_qty,approved_qty,po_issue_dt,po_approve_dt,sie_sale_ord,customer_no,net_price,total_price'
-                values1 = f'"{c.po_no}","{c.prod_id}","{c.order_qty}","{c.approved_qty}","{c.po_issue_dt}","{c.po_approve_dt}","{c.sie_sale_ord}","{c.customer_no}","{c.net_price}","{c.total_price}"'
+                values1 = f'"{c.po_no}","{c.prod_id}",{c.order_qty},{c.approved_qty},"{c.po_issue_dt}","{c.po_approve_dt}","{c.sie_sale_ord}","{c.customer_no}",{c.net_price},{c.total_price}'
                 table_name1 = "td_siemens_log"
-                whr1=   None
+                whr1=None
                 flag1 =  0
                 result1 = await db_Insert(table_name1, fields1, values1, whr1, flag1)
                 item_save=1 if result1['suc']>0 else 0
