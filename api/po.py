4ce511d44b578b9755cde83ba97cafe2e200f0ad
somnathsynergic <somnath@synergic.in>
2025-06-06 18:47:27 +0530

    commit with siemens

--- api/po.py
+++ api/po.py
@@ -4873,7 +4873,7 @@ async def getcategory(id:GetRows):
     select = "@a:=@a+1 serial_number, d.sl_no,d.po_no,d.created_by,d.created_at, pr.proj_name,pr.proj_id, 'A' as po_status,'Siemens' as vendor_name"
     schema = "td_siemens_details d join td_project pr on d.proj_id=pr.sl_no,(SELECT @a:= 0) AS a"
     where = f"d.sl_no='{id.id}'" if id.id>0 else f""
-    order = ""
+    order = "order by d.created_at desc"
     flag = 1
     result = await db_select(select, schema, where, order, flag)
     print(result, 'RESULT')
