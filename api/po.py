d2799580adedd62ab15d43daee8dc9ad3e45ce4a
somnathsynergic <somnath@synergic.in>
2025-07-17 16:34:20 +0530

    commit with report

--- api/po.py
+++ api/po.py
@@ -3283,7 +3283,7 @@ async def item_dtls(data:ProjId):
     flag3 = 1 
     res_dt3 = await db_select(select3,table3,where3,order3,flag3)
     
-    print(res_dt3)
+    print('res_dt3',res_dt3)
 
 
     select2 = f"c.prod_name, c.sl_no prod_id,c.article_no,c.part_no,c.model_no,c.part_no, sum(b.rc_qty) tot_rc_qty,(SELECT SUM(qty*in_out_flag) FROM `td_stock_new` WHERE item_id=c.sl_no and proj_id={data.Proj_id}) as project_stock, (SELECT SUM(qty*in_out_flag) FROM `td_stock_new` WHERE item_id=c.sl_no and proj_id='0') as warehouse_stock,(select sum(req_qty) from td_requisition_items where project_id={data.Proj_id} and item_id=d.prod_id) as tot_req,(select sum(qty) from td_stock_new where proj_id={data.Proj_id} and item_id=c.sl_no and in_out_flag=-1 and ref_no not like '%T%') as tot_del"
@@ -3293,7 +3293,7 @@ async def item_dtls(data:ProjId):
     flag2 = 1 
     res_dt2 = await db_select(select2,table2,where2,order2,flag2)
 
-    print(res_dt1,res_dt2)
+    # print(res_dt1,res_dt2)
 
     res_dt = {'suc':1, 'msg':res_dt1['msg']+res_dt2['msg']+res_dt3['msg']}
     return res_dt
