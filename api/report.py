72de3f8e6a5ad97317d5cbb6e37261ff184f0446
somnathsynergic <somnath@synergic.in>
2025-09-24 10:58:39 +0530

    commit with report

--- api/report.py
+++ api/report.py
@@ -863,11 +863,18 @@ async def getprojectpoc(id:MatVal):
     # res_dt = {}
 
     select = f"@a:=@a+1 '#',concat(pd.prod_name,'(Part No.:',pd.part_no,', Article No.:',pd.article_no,', Model No.:',pd.model_no,', Make:',pd.prod_make,', Description:',pd.prod_desc,')') as 'Product',GROUP_CONCAT(m.invoice  SEPARATOR '\n') as 'Invoice',GROUP_CONCAT(DATE_FORMAT(m.invoice_dt,'%d/%m/%Y')  SEPARATOR '\n') as 'Invoice Date',i.cgst_id as 'CGST',i.sgst_id as 'SGST',i.igst_id 'IGST',(i.item_rt-i.discount)*sum(st.qty) as 'Net Unit Price', IF(i.igst_id > 0,( i.item_rt - i.discount ) * sum(st.qty*st.in_out_flag) * i.igst_id / 100+((i.item_rt-i.discount)*sum(st.qty*st.in_out_flag)), ( ((i.item_rt - i.discount ) *sum(st.qty*st.in_out_flag) * i.cgst_id / 100)+((i.item_rt-i.discount)*sum(st.qty*st.in_out_flag)) ) + (( (i.item_rt - i.discount ) * sum(st.qty*st.in_out_flag) * i.sgst_id / 100 ))) AS Total,sum(st.qty*st.in_out_flag) as 'Quantity', SUM(d.rc_qty) AS 'Received Quantity', SUM(st.qty*st.in_out_flag) AS 'Stock In Quantity'"
+    # schema = '''td_po_basic b 
+    # left join td_po_items i on b.sl_no=i.po_sl_no 
+    # left join md_product pd on pd.sl_no=i.item_id
+    # join td_item_delivery_details d on d.po_no=b.po_no and d.prod_id=i.item_id 
+    # join td_stock_new st on st.item_id=d.prod_id and st.ref_no=d.mrn_no 
+    # join td_item_delivery_invoice m on m.mrn_no=d.mrn_no,(SELECT @a:= 0) AS a
+    # '''
     schema = '''td_po_basic b 
     left join td_po_items i on b.sl_no=i.po_sl_no 
     left join md_product pd on pd.sl_no=i.item_id
     join td_item_delivery_details d on d.po_no=b.po_no and d.prod_id=i.item_id 
-    join td_stock_new st on st.item_id=d.prod_id and st.ref_no=d.mrn_no 
+    join td_stock_new st on st.item_id=d.prod_id
     join td_item_delivery_invoice m on m.mrn_no=d.mrn_no,(SELECT @a:= 0) AS a
     '''
    
